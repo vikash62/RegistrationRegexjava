bf801ba2ad66dd468265163ee7a2efe4faeb0e60
Vikash <vikashpathak041@gmail.com>
2022-06-21 18:06:56 +0530

    Enter valid password at least 1 numeric password

--- src/com/bridgelabz/UserRegistration/RegexExpression.java
+++ src/com/bridgelabz/UserRegistration/RegexExpression.java
@@ -39,7 +39,7 @@ public class RegexExpression
         return false;
     }
     public boolean password(String password) {
-        String pattern  = "^[A-Z]{1}[A-Za-z0-9]{7,}$";
+        String pattern  = "(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}";
         if (password.matches(pattern)) {
             return true;
         }
