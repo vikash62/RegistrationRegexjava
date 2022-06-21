937e84cbd7f79baf839944d28896eaba95bf00cf
Vikash <vikashpathak041@gmail.com>
2022-06-21 17:54:48 +0530

    Enter valid last name

--- src/com/bridgelabz/UserRegistration/RegexExpression.java
+++ src/com/bridgelabz/UserRegistration/RegexExpression.java
@@ -4,18 +4,35 @@ import java.util.Scanner;
 
 public class RegexExpression
 {
-    static String firstName = "^[A-Z]{1}[A-Za-z]{2,}$";
-
+    public boolean firstName(String name)
+    {
+        String firstName = "^[A-Z]{1}[A-Za-z]{2,}$";
+        if (name.matches(firstName))
+        {
+            return true;
+        }
+        return false;
+    }
+    public boolean lastName(String name)
+    {
+        String lastName = "^[A-Z]{1}[A-Za-z]{2,}$";
+        if (name.matches(lastName))
+        {
+            return true;
+        }
+        return false;
+    }
     public static void main(String[] args)
     {
+        RegexExpression regex = new RegexExpression();
         Scanner scan = new Scanner(System.in);
+
         System.out.println("Enter the first name :");
-        String testName = scan.nextLine();
-        if(testName.matches(firstName))
-        {
-            System.out.println("Thi is valid first name .");
-        }
-        else {
-            System.out.println("Not a valid name");
-        }
-    }   
+        String firstName = scan.nextLine();
+        System.out.println(regex.firstName(firstName));
+
+        System.out.println("Enter the last name :");
+        String lastName = scan.nextLine();
+        System.out.println(regex.firstName(lastName));
+    }
+}
