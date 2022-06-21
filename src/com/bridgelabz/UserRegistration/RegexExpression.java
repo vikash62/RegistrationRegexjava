6ac2b97670cb37bce7b7bd6bfdbf2f00e45b1704
Vikash <vikashpathak041@gmail.com>
2022-06-21 17:49:57 +0530

    some msg

---
+++ src/com/bridgelabz/UserRegistration/RegexExpression.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.UserRegistration;
+
+import java.util.Scanner;
+
+public class RegexExpression
+{
+    static String firstName = "^[A-Z]{1}[A-Za-z]{2,}$";
+
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the first name :");
+        String testName = scan.nextLine();
+        if(testName.matches(firstName))
+        {
+            System.out.println("Thi is valid first name .");
+        }
+        else {
+            System.out.println("Not a valid name");
+        }
+    }
+}
