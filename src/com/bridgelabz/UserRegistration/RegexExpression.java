348c8a2a3bf4d5867781391189143d10b5dc3729
Vikash <vikashpathak041@gmail.com>
2022-06-21 18:04:07 +0530

    Enter valid password at least 1 character

--- src/com/bridgelabz/UserRegistration/RegexExpression.java
+++ src/com/bridgelabz/UserRegistration/RegexExpression.java
@@ -39,7 +39,7 @@ public class RegexExpression
         return false;
     }
     public boolean password(String password) {
-        String pattern  = "^[a-zA-Z0-9]{8,}$";
+        String pattern  = "^[A-Z]{1}[A-Za-z0-9]{7,}$";
         if (password.matches(pattern)) {
             return true;
         }
