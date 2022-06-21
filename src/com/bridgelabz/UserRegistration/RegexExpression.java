1dcda2bbea61884ed564fea878e39ba234586745
Vikash <vikashpathak041@gmail.com>
2022-06-21 17:59:08 +0530

    Enter valid mobile no

--- src/com/bridgelabz/UserRegistration/RegexExpression.java
+++ src/com/bridgelabz/UserRegistration/RegexExpression.java
@@ -31,6 +31,13 @@ public class RegexExpression
         }
         return false;
     }
+    public boolean mobile(String mobile) {
+        String pattern  = "^(91)[\\s]{1}[1-9]{1}[0-9]{9}$";
+        if (mobile.matches(pattern)) {
+            return true;
+        }
+        return false;
+    }
     public static void main(String[] args)
     {
         RegexExpression regex = new RegexExpression();
@@ -47,5 +54,9 @@ public class RegexExpression
         System.out.println("Enter the email address:");
         String email = scan.nextLine();
         System.out.println(regex.email(email));
+
+        System.out.println("Enter the mobile number:");
+        String mobile = scan.nextLine();
+        System.out.println(regex.mobile(mobile));
     }
 }
